f3a7792cd96016cef89f52395f1dc8c081f84dc3
ItoSbai <lualaa.design2565@gmail.com>
2024-03-02 11:45:35 +0900

    Revert "元のリポジトリからfrontendのコードを切り分け"
    This reverts commit b6fc109a1c75110fbb81f0d5d34b04c3d10f2488, reversing
    changes made to 127b30b910297a16cd13f0c7cfed655eefb8fade.

--- src/components/MarkerComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Marker } from '@vis.gl/react-google-maps';
-
-const MarkerComponent = ({ position }) => {
-  return <Marker position={position} />;
-};
-
-export default MarkerComponent;
